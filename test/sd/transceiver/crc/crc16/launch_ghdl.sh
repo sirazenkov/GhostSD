769a9b547abfca4c5f7233aa2b24bf2ddd4e9b87
Simon Razenkov <sirazenkov@stud.tsu.ru>
2022-09-15 12:42:33 +0700

    Fixed crc test files

--- test/sd/transceiver/crc/crc16/launch_ghdl.sh
+++ test/sd/transceiver/crc/crc16/launch_ghdl.sh
@@ -2,6 +2,6 @@ mkdir work
 
 ghdl -a --workdir=work \
 	crc16_tb.vhd \
-	../../../../src/sd/crc/crc16.vhd
+	../../../../../src/sd/transceiver/crc/crc16.vhd
 
 ghdl -r --workdir=work crc16_tb --wave=./work/wave.ghw
--- test/sd/transceiver/crc/crc7/launch_ghdl.sh
+++ test/sd/transceiver/crc/crc7/launch_ghdl.sh
@@ -2,6 +2,6 @@ mkdir work
 
 ghdl -a --workdir=work \
 	crc7_tb.vhd \
-	../../../../src/sd/crc/crc7.vhd
+	../../../../../src/sd/transceiver/crc/crc7.vhd
 
 ghdl -r --workdir=work crc7_tb --wave=./work/wave.ghw
