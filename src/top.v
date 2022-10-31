79095564f9bf8674fc209e09ac71a40077e69fc1
Simon Razenkov <sirazenkov@stud.tsu.ru>
2022-10-31 10:49:20 +0700

    Added top module

---
+++ src/top.v
@@ -0,0 +1,24 @@
+//=======================================================================
+//company: Tomsk State University
+//developer: Simon Razenkov
+//e-mail: sirazenkov@stud.tsu.ru
+//description: Top module of GhostSD project
+//=======================================================================
+
+module top
+	(
+	input iclk, // 72 MHz system clock
+	
+	// SD lines
+	inout iocmd_sd,		// CMD line
+	inout [3:0] iodata_sd,	// D[3:0] line
+	output oclk_sd,		// CLK line
+
+	output status_led	// Blinking - running operation
+				// OFF - failed
+				// ON - success
+	);
+
+	
+
+endmodule
