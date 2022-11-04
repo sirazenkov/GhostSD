698cea72c569979cfad8825d865dadc868677bb2
Simon Razenkov <sirazenkov@stud.tsu.ru>
2022-11-04 17:55:51 +0700

    Added response check in cmd_driver testbench

--- test/sd/transceiver/line_drivers/cmd_driver/cmd_driver_tb.v
+++ test/sd/transceiver/line_drivers/cmd_driver/cmd_driver_tb.v
@@ -15,7 +15,7 @@ module cmd_driver_tb;
 	reg [31:0] cmd_arg;
 
 	reg [46:0] correct_cmd;
-	reg [119:0] resp_reg;
+	reg [135:0] correct_resp;
 
 	wire [119:0] resp;
 	wire cmd_sd, done;
@@ -55,29 +55,46 @@ module cmd_driver_tb;
 
 	initial
 	begin
-		$display("Starting cmd_driver testbench...");
-
 		// Reset
 		#HALF_PERIOD;		
 		rst = 1'b1;
 		#PERIOD;		
 		rst = 1'b0;
 
+		// Start
+		start = 1'b1;
+	end
+
+	initial
+	begin
+		$display("Starting cmd_driver testbench...");
+
 		cmd_index = 6'b010001;
 		cmd_arg = {32{1'b0}};
 		correct_cmd = {1'b1, 6'b010001, {32{1'b0}}, 7'b0101010, 1'b1};
 
-		start = 1'b1;
-
 		@(cmd_sd == 1'b0);
-		#PERIOD;
+		#HALF_PERIOD;		
 		for(i = 46; i >= 0; i = i - 1)
 		begin
 			#PERIOD;
 			if(cmd_sd != correct_cmd[i])
 				$display("Command check failed on bit %d", i);
 		end
-		#PERIOD;
+
+		correct_resp = {1'b0, 1'b0, 6'b010001, 32'h00000900, 7'b0110011, 1'b1, {88{1'b0}}};
+		for(i = 135; i > 88; i = i - 1)
+		begin
+			#PERIOD;
+			cmd_sd_reg = correct_resp[i];
+		end
+
+		@(done == 1'b1);
+		cmd_sd_reg = 1'bz;
+
+		if(resp != {{82{1'b0}}, correct_resp[133:96]})
+			$display("Wrong response!");
+
 		$display("End of test");
 		$finish;
 	end
