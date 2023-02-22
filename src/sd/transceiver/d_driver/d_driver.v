223e782a5717088c74e13cec26d91c91954d0411
Simon Razenkov <semen.razenkov@yandex.ru>
2023-02-23 02:25:08 +0700

    Finished testing and fixing d_driver

--- src/sd/transceiver/d_driver/d_driver.v
+++ src/sd/transceiver/d_driver/d_driver.v
@@ -24,7 +24,7 @@ module d_driver (
   // RAM with processed data (for sending)
   input [3:0] irdata,
 
-  output ocrc_fail,
+  output reg ocrc_fail,
   output odone
 );
 
@@ -47,16 +47,17 @@ module d_driver (
     BUSY      = 3'b111;
   reg [2:0] state = IDLE, next_state;
 
-  reg [3:0] data     =  4'h0;
-  reg [9:0] counter  = 10'd0;
-  reg       crc_fail =  1'b0;
+  reg [3:0]  data;
+  reg [10:0] counter  = 11'd0;
 
   reg unload = 1'b0;
 
   wire rst_crc;
   assign rst_crc = irst || state == IDLE || state == WAIT_SEND || state == WAIT_RCV;
 
+  wire [3:0] data_crc;
   wire [3:0] crc;
+  assign data_crc = state == SEND_DATA ? irdata : data;
 
   genvar i;
   generate
@@ -65,7 +66,7 @@ module d_driver (
         .irst(rst_crc),
         .iclk(iclk),
 
-        .idata(data[i]),
+        .idata(data_crc[i]),
 
         .iunload(unload),
         .ocrc(crc[i])
@@ -73,14 +74,13 @@ module d_driver (
     end
   endgenerate
 
-  assign odata_sd = state == SEND_DATA || state == SEND_CRC ? data : 4'hF;
+  assign odata_sd = next_state == SEND_DATA || state == SEND_DATA || state == SEND_CRC ? data : 4'hF;
 
   assign owdata    = data;
   assign oaddr     = counter;
   assign owrite_en = state == RCV_DATA;
 
-  assign ocrc_fail = crc_fail;
-  assign odone     = state == IDLE || state == WAIT_SEND;
+  assign odone = state == IDLE || state == WAIT_SEND;
 
   always @(posedge iclk)
     state <= irst ? IDLE : next_state;
@@ -89,75 +89,75 @@ module d_driver (
     next_state = state;
     case(state)
       IDLE:      if (istart)            next_state = WAIT_RCV;
-      WAIT_RCV:  if (data == 4'h0)      next_state = RCV_DATA;
-      RCV_DATA:  if (&counter)          next_state = CHECK_CRC;
+      WAIT_RCV:  if (~|data)            next_state = RCV_DATA;
+      RCV_DATA:  if (&counter[9:0])     next_state = CHECK_CRC;
       CHECK_CRC:
                  if (counter == 10'd15) next_state = WAIT_SEND;
-		 else if  (crc != data) next_state = IDLE;
+		 else if (crc != data)  next_state = IDLE;
       WAIT_SEND: if (istart)            next_state = SEND_DATA; // Wait until data is processed
-      SEND_DATA: if (&counter)          next_state = SEND_CRC;
-      SEND_CRC:  if (counter == 10'd15) next_state = BUSY;
-      BUSY:      if (idata_sd[0])       next_state = IDLE;      // SD card finished writing 
+      SEND_DATA: if (counter[10])       next_state = SEND_CRC;
+      SEND_CRC:  if (counter == 11'd16) next_state = BUSY;
+      BUSY:      if (data[0])           next_state = IDLE;      // SD card finished writing 
       default: next_state = IDLE;
     endcase
   end
 
   always @(posedge iclk) begin
-    if (irst)  
+    if (irst)
       data <= 4'h0;
-    if (state == IDLE || WAIT_RCV || state == RCV_DATA || state == CHECK_CRC)
-      data <= idata_sd;
-    else if (istart && state == WAIT_SEND)
-      data <= 4'h0;  // Send start bit
-    else if (state == SEND_DATA && &counter)
-      data <= crc;  
-    else if (state == SEND_CRC && counter == 10'd15)
-      data <= 4'hf;  // Send end bit
-    else
+    else if (state == WAIT_SEND && next_state == SEND_DATA)
+      data <= 4'h0;
+    else if (state == SEND_DATA)
       data <= irdata;
+    else if (state == SEND_CRC)
+      data <= crc;
+    else
+      data <= idata_sd;
   end
 
   always @(posedge iclk) begin
     if (irst) begin
-      counter  <= 10'd0;
-      unload   <=  1'b0;
-      crc_fail <=  1'b0;
+      counter   <= 11'd0;
+      unload    <=  1'b0;
+      ocrc_fail <=  1'b0;
     end
     else begin
       case(state)
         IDLE: begin
-          if (istart) begin
-            counter  <= 10'd0;
-            crc_fail <=  1'b0;
+          if (next_state == RCV_DATA) begin
+            ocrc_fail <=  1'b0;
           end
         end
         RCV_DATA: begin
           counter <= counter + 1'b1;
-          if (&counter) begin
-            counter <= 10'd0;
+          if (next_state == CHECK_CRC) begin
+            counter <= 11'd0;
             unload  <=  1'b1;
           end
         end
 	CHECK_CRC: begin // Check CRC on the data lines
-          counter <= counter + 1'b1;
-          if (crc != data)
-            crc_fail <= 1'b1;
+	  if (next_state == WAIT_SEND) begin
+	    counter <= 11'd0;
+	    unload  <= 1'b0;
+	  end else counter <= counter + 1'b1;
+	  if (next_state == IDLE) ocrc_fail <= 1'b1;
         end
-	WAIT_SEND: begin
-          if (istart)
-            counter <= 10'd0;
+        WAIT_SEND: begin
+	  if (next_state == SEND_DATA) counter <= counter + 1;
         end
-        SEND_DATA: begin
+	SEND_DATA: begin
           counter <= counter + 1'b1;
-          if (&counter) begin
-            unload  <=  1'b1;
-            counter <= 10'd0;
+          if (next_state == SEND_CRC) begin
+            counter <= 11'd0;
+            unload  <= 1'b1;
           end
         end
         SEND_CRC: begin
           counter <= counter + 1'b1;
-          if (counter == 10'd15)
-            unload <= 1'b0;
+	  if (next_state == BUSY) begin
+            counter <= 11'd0;
+            unload  <= 1'b0;
+	  end
         end
       endcase
     end
--- test/sd/transceiver/d_driver/d_driver_tb.py
+++ test/sd/transceiver/d_driver/d_driver_tb.py
@@ -1,10 +1,12 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import FallingEdge, RisingEdge
+from cocotb.triggers import FallingEdge, RisingEdge, ClockCycles
 
 import logging
 
 import random
+
+NUM_OF_TRANSACTIONS = 5
 
 logging.basicConfig(level=logging.NOTSET)
 logger = logging.getLogger()
@@ -35,32 +37,8 @@ class D_driver_BFM():
                 mem[int(self.dut.oaddr.value)] = int(self.dut.owdata.value)
             self.dut.irdata.value = mem[int(self.dut.oaddr.value)]
 
-    def crc16(self, data):
-        crc = 0
-        for i in range(1024): 
-            data_bit = (data >> (1023-i)) & 1
-            last_bit = (crc >> 15) & 1
-            xor_bit = last_bit ^ data_bit
-            crc = crc << 1
-            crc = crc & ((1 << 16) - 1)
-            crc = crc ^ xor_bit ^ (xor_bit << 5) ^ (xor_bit << 12)
-        return crc
-
-    def crc_packets(self, block):
-        lines_data = [0,0,0,0]
-        for i in range(1024):
-            for j in range(4):
-                lines_data[j] = lines_data[j] << 1 | ((block[i] >> j) & 1)
-        crc_values = [self.crc16(line_data) for line_data in lines_data]
-        crc_packets = []
-        for i in range(16):
-            crc_packet = 0
-            for j in range(4):
-                crc_packet = crc_packet | (((crc_values[j] >> (15-i)) & 1) << j)
-            crc_packets.append(crc_packet)
-        return crc_packets
-
-    async def send_block(self, block):
+    async def send_block(self, block, crc_packets):
+        await FallingEdge(self.dut.iclk)
         self.dut.istart.value = 1
         await FallingEdge(self.dut.iclk)
         self.dut.idata_sd.value = 0 # Start bit
@@ -69,25 +47,61 @@ class D_driver_BFM():
         for i in range(1024):
             self.dut.idata_sd.value = block[i]
             await FallingEdge(self.dut.iclk)
-        crc_packets = self.crc_packets(block)
         for i in range(16):
             self.dut.idata_sd.value = crc_packets[i]
             await FallingEdge(self.dut.iclk)
         self.dut.idata_sd.value = 0xF # End bit
+        await RisingEdge(self.dut.odone)
         await FallingEdge(self.dut.iclk)
 
-    async def receive_block(self):
+    async def receive_block(self, crc_packets):
+        await FallingEdge(self.dut.iclk)
         self.dut.istart.value = 1
-        while(self.dut.odata_sd.value != 0): # Start bit
-            await FallingEdge(self.dut.iclk)
+        await FallingEdge(self.dut.iclk)
         self.dut.istart.value = 0
         await FallingEdge(self.dut.iclk)
         block = []
         for i in range(1024):
             block.append(int(self.dut.odata_sd))
             await FallingEdge(self.dut.iclk)
-        return block
- 
+        crc_failed = False
+        for i in range(16):
+            if(int(self.dut.odata_sd.value) != crc_packets[i]):
+                crc_failed = True
+            await FallingEdge(self.dut.iclk)
+        await RisingEdge(self.dut.odone)
+        return (block, crc_failed)
+
+    async def random_delay(self, upper_bound):
+        delay = random.randint(0,upper_bound)
+        await ClockCycles(self.dut.iclk, delay)
+
+
+def crc16(data):
+    crc = 0
+    for i in range(1024): 
+        data_bit = (data >> (1023-i)) & 1
+        last_bit = (crc >> 15) & 1
+        xor_bit = last_bit ^ data_bit
+        crc = crc << 1
+        crc = crc & ((1 << 16) - 1)
+        crc = crc ^ xor_bit ^ (xor_bit << 5) ^ (xor_bit << 12)
+    return crc
+
+def gen_crc_packets(block):
+    lines_data = [0,0,0,0]
+    for i in range(1024):
+        for j in range(4):
+            lines_data[j] = lines_data[j] << 1 | ((block[i] >> j) & 1)
+    crc_values = [crc16(line_data) for line_data in lines_data]
+    crc_packets = []
+    for i in range(16):
+        crc_packet = 0
+        for j in range(4):
+            crc_packet = crc_packet | (((crc_values[j] >> (15-i)) & 1) << j)
+        crc_packets.append(crc_packet)
+    return crc_packets
+
 @cocotb.test()
 async def d_driver_tb(_):
     """D line's driver testbench""" 
@@ -96,10 +110,11 @@ async def d_driver_tb(_):
     await bfm.start_operation()
     await bfm.reset()
 
-    transactions = random.randint(0,5)
-    for i in range(transactions):
+    for i in range(NUM_OF_TRANSACTIONS):
         block = [random.randint(0,15) for i in range(1024)]
-        await bfm.send_block(block)
+        crc_packets = gen_crc_packets(block)
+        await bfm.random_delay(10)
+        await bfm.send_block(block, crc_packets)
         if(int(bfm.dut.ocrc_fail.value) == 0):
             logger.info(f"CRC16 check for block {i} succeeded!")
         else:
@@ -112,17 +127,23 @@ async def d_driver_tb(_):
             logger.error(f"Module did not receive block {i} successfully!")
             passed = False
             break
-        received_block = await bfm.receive_block()
-        if(int(bfm.dut.odata_sd.value) == 0xF):
-            logger.info(f"Finish bit set in block {i}!") 
+        await bfm.random_delay(10)
+        received_block, crc_failed = await bfm.receive_block(crc_packets)
+        if(block == received_block):
+            logger.info(f"Block {i} from module is equal the initial block {i}!") 
         else:
-            logger.error(f"Finish bit not set in block {i}!")
+            logger.error(f"Block {i} from module does not equal the initial block {i}!")
             passed = False
             break
-        if(block == received_block):
-            logger.info(f"Received block {i} is equal the initial block {i}!") 
+        if(not crc_failed):
+            logger.info(f"Correct CRC in block {i} from the module!")
         else:
-            logger.error(f"Received block {i} does not equal the initial block {i}!")
+            logger.error(f"Wrong CRC in block {i} from the module!")
+            break
+        if(int(bfm.dut.odata_sd.value) == 0xF):
+            logger.info(f"Finish bit set in block {i} from module!") 
+        else:
+            logger.error(f"Finish bit not set in block {i} from module!")
             passed = False
             break
     assert passed
