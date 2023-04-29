de8ab7fd251346d0691d20dcf2759590c72f6150
Simon Razenkov <semen.razenkov@yandex.ru>
2023-04-29 20:18:26 +0700

    Added OTP clock in iCE40 project

--- src/clock_divider.v
+++ src/clock_divider.v
@@ -9,6 +9,7 @@ module clock_divider (
   input irst,      // Global reset
   input iclk,      // Reference clock
   input isel_clk,  // Select slow (0) or fast (1) clock
+  output oclk_otp,
   output oclk_sd
 );
 
@@ -20,6 +21,7 @@ module clock_divider (
       counter <= counter + 1'b1;  
   end
 
-  assign oclk_sd = isel_clk ? counter[0] : counter[6];
+  assign oclk_sd  = isel_clk ? counter[0] : counter[6];
+  assign oclk_otp = iclk;
 
 endmodule
