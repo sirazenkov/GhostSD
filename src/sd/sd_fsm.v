84d4f0282323c85c9bb713c2dccd388cdc2ada1b
sirazenkov <semen.razenkov@yandex.ru>
2023-05-15 22:22:49 +0700

    Optimized SD FSM output logic

--- src/sd/sd_fsm.v
+++ src/sd/sd_fsm.v
@@ -217,9 +217,9 @@ module sd_fsm
       if (next_state == CMD12) ogen_otp <= state == WRITE ? 1'b1 : 1'b0;
     end else begin
       ostart_cmd <= icmd_done && (next_state == CMD13 || next_state == CMD6) ? 1'b1 : 1'b0;
-      ostatus_d  <= icmd_done && state == CMD6 && next_state == CMD6 ? 1'b1 : 1'b0;
+      ostatus_d  <= icmd_done && state == CMD6 ? 1'b1 : 1'b0;
       ostart_d   <= 1'b0;
-      ogen_otp   <= icmd_done && state == CMD6 && next_state == CMD6 ? 1'b1 : 1'b0;
+      ogen_otp   <= icmd_done && state == CMD6 ? 1'b1 : 1'b0;
     end
   end
   
