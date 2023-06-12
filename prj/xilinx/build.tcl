7c3c72dc56e71fa0ef085eeb07663921a75078ed
sirazenkov <sirazenkov@stud.tsu.ru>
2023-06-13 02:22:46 +0700

    Refactored Xilinx flow

--- prj/xilinx/build.tcl
+++ prj/xilinx/build.tcl
@@ -1,12 +1,14 @@
 open_project ghost_sd.xpr
 update_compile_order -fileset sources_1
 
+set_param general.maxThreads 32
+
 reset_run synth_1
-launch_runs synth_1 -jobs 4
+launch_runs synth_1
 wait_on_run synth_1
 
 reset_run impl_1
-launch_runs impl_1 -to_step write_bitstream -jobs 4
+launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 
 close_project
